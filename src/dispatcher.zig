b2f71d20cca76d8ea64b07b8e8d42e58f492bfab
William Wong <williamw520@gmail.com>
2025-04-23 19:01:25 -0700

    Simplify HandlerFn declaration.

--- src/dispatcher.zig
+++ src/dispatcher.zig
@@ -194,38 +194,30 @@ const HandlerInfo = struct {
     raw_params: bool,
 };
 
+// 4 JSON primitive types.
+// bool: bool,
+// integer: i64,
+// float: f64,
+// string: []const u8,
+
+
 /// The returned JSON string must be allocated with the passed in allocator.
 /// The caller will free it with the allocator after using it in the Response message.
 /// Call std.json.stringifyAlloc() to build the returned JSON will take care of it.
-const Handler0 = *const fn(Allocator) anyerror![]const u8;
-const Handler1 = *const fn(Allocator, Value) anyerror![]const u8;
-const Handler2 = *const fn(Allocator, Value, Value) anyerror![]const u8;
-const Handler3 = *const fn(Allocator, Value, Value, Value) anyerror![]const u8;
-const Handler4 = *const fn(Allocator, Value, Value, Value, Value) anyerror![]const u8;
-const Handler5 = *const fn(Allocator, Value, Value, Value, Value, Value) anyerror![]const u8;
-const Handler6 = *const fn(Allocator, Value, Value, Value, Value, Value, Value) anyerror![]const u8;
-const Handler7 = *const fn(Allocator, Value, Value, Value, Value, Value, Value, Value) anyerror![]const u8;
-const Handler8 = *const fn(Allocator, Value, Value, Value, Value, Value, Value, Value, Value) anyerror![]const u8;
-const Handler9 = *const fn(Allocator, Value, Value, Value, Value, Value, Value, Value, Value, Value) anyerror![]const u8;
-const HandlerArr = *const fn(Allocator, Array) anyerror![]const u8;
-const HandlerObj = *const fn(Allocator, ObjectMap) anyerror![]const u8;
-const HandlerVal = *const fn(Allocator, Value) anyerror![]const u8;
-
-// Use tagged union to wrap different types of handler.
 const HandlerFn = union(enum) {
-    fn0: Handler0,
-    fn1: Handler1,
-    fn2: Handler2,
-    fn3: Handler3,
-    fn4: Handler4,
-    fn5: Handler5,
-    fn6: Handler6,
-    fn7: Handler7,
-    fn8: Handler8,
-    fn9: Handler9,
-    fnArr: HandlerArr,
-    fnObj: HandlerObj,
-    fnVal: HandlerVal,
+    fn0: *const fn(Allocator) anyerror![]const u8,
+    fn1: *const fn(Allocator, Value) anyerror![]const u8,
+    fn2: *const fn(Allocator, Value, Value) anyerror![]const u8,
+    fn3: *const fn(Allocator, Value, Value, Value) anyerror![]const u8,
+    fn4: *const fn(Allocator, Value, Value, Value, Value) anyerror![]const u8,
+    fn5: *const fn(Allocator, Value, Value, Value, Value, Value) anyerror![]const u8,
+    fn6: *const fn(Allocator, Value, Value, Value, Value, Value, Value) anyerror![]const u8,
+    fn7: *const fn(Allocator, Value, Value, Value, Value, Value, Value, Value) anyerror![]const u8,
+    fn8: *const fn(Allocator, Value, Value, Value, Value, Value, Value, Value, Value) anyerror![]const u8,
+    fn9: *const fn(Allocator, Value, Value, Value, Value, Value, Value, Value, Value, Value) anyerror![]const u8,
+    fnArr: *const fn(Allocator, Array) anyerror![]const u8,
+    fnObj: *const fn(Allocator, ObjectMap) anyerror![]const u8,
+    fnVal: *const fn(Allocator, Value) anyerror![]const u8,
 };
 
 fn toHandlerFn(handler_fn: anytype, opt: RegisterOptions) !HandlerFn {
