596fb7352fa4df3d3dd5d6f3db64f7ca992b4a5c
William Wong <williamw520@gmail.com>
2025-05-07 16:42:57 -0700

    Add delimiter based response streaming API. Add Content-Length header based response streaming API.

--- src/streaming/stream.zig
+++ src/streaming/stream.zig
@@ -48,16 +48,15 @@ pub fn delimiterRequestStream(alloc: Allocator, comptime read_delimiter: u8, com
     }
 }
 
-
 /// Provides frame level support for JSON-RPC streaming based on Content-Length header.
 /// The message frame has the format of:
 ///     Content-Length: MESSAGE_LENGTH\r\n
 ///     \r\n\r\n
-///     JSON-RPC message
+///     JSON-RPC request message
 /// Runs a loop to read a stream of JSON request messages (frames) from the reader,
 /// handle each one with the dispatcher, and write the JSON responses to the buffered_writer.
 /// Each incoming message frame from the reader has a Content-Length header.
-/// Each outgoing message frame to the write_delimiter has a Content-Length header.
+/// Each outgoing message frame to the buffered_writer has a Content-Length header.
 pub fn lengthRequestStream(alloc: Allocator, reader: anytype, buffered_writer: anytype,
                            dispatcher: anytype) !void {
     var msg_buf = std.ArrayList(u8).init(alloc);
@@ -78,6 +77,58 @@ pub fn lengthRequestStream(alloc: Allocator, reader: anytype, buffered_writer: a
             try buffered_writer.flush();
             alloc.free(result_json);
         }
+    }
+}
+
+
+/// Provides framing level support for JSON-RPC streaming based on frame delimiter.
+/// Runs a loop to read a stream of JSON response messages (frames) from the reader,
+/// handle each one with the dispatcher.
+/// The incoming framed messages are delimitered by the read_delimiter.
+/// A typical JSON-RPC stream is delimitered by '\n' the CR character,
+/// which reqires all message content does not content the character.
+pub fn delimiterResponseStream(alloc: Allocator, comptime read_delimiter: u8,
+                               reader: anytype, dispatcher: anytype) !void {
+    var json_frame = std.ArrayList(u8).init(alloc); // one frame is one JSON response.
+    defer json_frame.deinit();
+    var buffered_reader = std.io.bufferedReader(reader);
+    var buf_reader = buffered_reader.reader();
+
+    while (true) {
+        json_frame.clearRetainingCapacity();
+        _ = buf_reader.streamUntilDelimiter(json_frame.writer(), read_delimiter, null) catch |err| {
+            switch (err) {
+                error.EndOfStream => break,
+                else => return err,
+            }
+        };
+
+        try runner.runResponseJson(alloc, json_frame.items, dispatcher);
+    }
+}
+
+/// Provides frame level support for JSON-RPC streaming based on Content-Length header.
+/// The message frame has the format of:
+///     Content-Length: MESSAGE_LENGTH\r\n
+///     \r\n\r\n
+///     JSON-RPC response message
+/// Runs a loop to read a stream of JSON response messages (frames) from the reader,
+/// and handle each one with the dispatcher.
+/// Each incoming message frame from the reader has a Content-Length header.
+pub fn lengthResponseStream(alloc: Allocator, reader: anytype, dispatcher: anytype) !void {
+    var msg_buf = std.ArrayList(u8).init(alloc);
+    defer msg_buf.deinit();
+    var buffered_reader = std.io.bufferedReader(reader);
+    const buf_reader = buffered_reader.reader();
+
+    while (true) {
+        const msg_len = frame.readContentLengthFrame(buf_reader, &msg_buf) catch |err| {
+            if (err == error.EndOfStream)
+                break;
+            return err;
+        };
+        if (msg_len == 0) continue;     // skip empty content frame.
+        try runner.runResponseJson(alloc, msg_buf.items, dispatcher);
     }
 }
 
--- src/tests/stream_tests.zig
+++ src/tests/stream_tests.zig
@@ -66,6 +66,12 @@ const CounterDispatcher = struct {
     }
 };
 
+const ResponseDispatcher = struct {
+    pub fn run(_: Allocator, res: zigjr.RpcResponse) !void {
+        std.debug.print("RpcResponse: {any}\n", .{res});
+    }
+};
+
 
 test "delimiterRequestStream on JSON requests, single param, id" {
     const alloc = gpa.allocator();
@@ -131,13 +137,121 @@ test "lengthRequestStream on JSON requests, single param, id" {
         try stream.lengthRequestStream(alloc, reader, &buf_writer, &dispatcher);
         // std.debug.print("response_jsons: ##\n{s}##\n", .{write_buffer.items});
 
-        // try testing.expectEqualSlices(u8, write_buffer.items,
-        //     \\{ "jsonrpc": "2.0", "result": "abc", "id": "5a" }
-        //     \\{ "jsonrpc": "2.0", "result": "xyz", "id": "5b" }
-        //     \\{ "jsonrpc": "2.0", "id": "5c", "error": { "code": -32602, "message": "InvalidParams" } }
-        //     \\
-        // );
+        try testing.expectEqualSlices(u8, write_buffer.items,
+            \\Content-Length: 42
+            ++ "\r\n\r\n\r\n" ++
+            \\{ "jsonrpc": "2.0", "result": 1, "id": 2 }Content-Length: 88
+            ++ "\r\n\r\n\r\n" ++
+            \\{ "jsonrpc": "2.0", "id": 99, "error": { "code": -32601, "message": "MethodNotFound" } }Content-Length: 42
+            ++ "\r\n\r\n\r\n" ++
+            \\{ "jsonrpc": "2.0", "result": 0, "id": 4 }
+        );
+    }
+    if (gpa.detectLeaks()) std.debug.print("Memory leak detected!\n", .{});
+}
+
+test "delimiterResponseStream on JSON responses, single param, id" {
+    const alloc = gpa.allocator();
+    {
+        const req_jsons = 
+            \\{"jsonrpc": "2.0", "method": "fun0", "params": ["abc"], "id": "5a" }
+            \\{"jsonrpc": "2.0", "method": "fun0", "params": ["xyz"],  "id": "5b" }
+            \\{"jsonrpc": "2.0", "method": "fun0", "id": "5c" }
+            \\
+        ;
+        // std.debug.print("req_jsons: |{s}|\n", .{req_jsons});
+        var json_stream = std.io.fixedBufferStream(req_jsons);
+        const reader = json_stream.reader();
+
+        var write_buffer = ArrayList(u8).init(alloc);
+        defer write_buffer.deinit();
+        const writer = write_buffer.writer();
+        var buf_writer = std.io.bufferedWriter(writer);
+
+        try stream.delimiterRequestStream(alloc, '\n', '\n', reader, &buf_writer, EchoDispatcher);
+        // std.debug.print("output_jsons: ##\n{s}##\n", .{write_buffer.items});
+
+        try testing.expectEqualSlices(u8, write_buffer.items,
+            \\{ "jsonrpc": "2.0", "result": "abc", "id": "5a" }
+            \\{ "jsonrpc": "2.0", "result": "xyz", "id": "5b" }
+            \\{ "jsonrpc": "2.0", "id": "5c", "error": { "code": -32602, "message": "InvalidParams" } }
+            \\
+        );
+
+        var response_stream = std.io.fixedBufferStream(write_buffer.items);
+        const response_reader = response_stream.reader();
+        try stream.delimiterResponseStream(alloc, '\n', response_reader, struct {
+            pub fn run(_: Allocator, res: zigjr.RpcResponse) !void {
+                // std.debug.print("RpcResponse: {any}\n", .{res});
+                if (try res.id.eql("5a"))
+                    try testing.expectEqualSlices(u8, res.result.string, "abc");
+                if (try res.id.eql("5b"))
+                    try testing.expectEqualSlices(u8, res.result.string, "xyz");
+                if (try res.id.eql("5c"))
+                    try testing.expectEqual(res.err().code, @intFromEnum(ErrorCode.InvalidParams));
+            }
+        });
         
+    }
+    if (gpa.detectLeaks()) std.debug.print("Memory leak detected!\n", .{});
+}
+
+test "lengthResponseStream on JSON responses, single param, id" {
+    const alloc = gpa.allocator();
+    {
+        var dispatcher = CounterDispatcher{};
+        const req_jsons = [_][]const u8{
+            \\{"jsonrpc": "2.0", "method": "inc", "id": 1}
+            ,
+            \\{"jsonrpc": "2.0", "method": "get", "id": 2}
+            ,
+            \\{"jsonrpc": "2.0", "method": "dec", "id": 3}
+            ,
+            \\{"jsonrpc": "2.0", "method": "no-method", "id": 99}
+            ,
+            \\{"jsonrpc": "2.0", "method": "get", "id": 4}
+            ,
+        };
+        // std.debug.print("req_jsons: |{s}|\n", .{req_jsons});
+        const req_frames = try frame.writeContentLengthFrames(alloc, &req_jsons);
+        defer req_frames.deinit();
+        // std.debug.print("frames: |{s}|\n", .{req_frames.items});
+
+        var json_stream = std.io.fixedBufferStream(req_frames.items);
+        const reader = json_stream.reader();
+
+        var write_buffer = ArrayList(u8).init(alloc);
+        defer write_buffer.deinit();
+        const writer = write_buffer.writer();
+        var buf_writer = std.io.bufferedWriter(writer);
+
+        try stream.lengthRequestStream(alloc, reader, &buf_writer, &dispatcher);
+        // std.debug.print("request_jsons: ##\n{s}##\n", .{write_buffer.items});
+
+        try testing.expectEqualSlices(u8, write_buffer.items,
+            \\Content-Length: 42
+            ++ "\r\n\r\n\r\n" ++
+            \\{ "jsonrpc": "2.0", "result": 1, "id": 2 }Content-Length: 88
+            ++ "\r\n\r\n\r\n" ++
+            \\{ "jsonrpc": "2.0", "id": 99, "error": { "code": -32601, "message": "MethodNotFound" } }Content-Length: 42
+            ++ "\r\n\r\n\r\n" ++
+            \\{ "jsonrpc": "2.0", "result": 0, "id": 4 }
+        );
+
+        var response_stream = std.io.fixedBufferStream(write_buffer.items);
+        const response_reader = response_stream.reader();
+        try stream.lengthResponseStream(alloc, response_reader, struct {
+            pub fn run(_: Allocator, res: zigjr.RpcResponse) !void {
+                // std.debug.print("RpcResponse: {any}\n", .{res});
+                if (try res.id.eql(2))
+                    try testing.expectEqual(res.result.integer, 1);
+                if (try res.id.eql(4))
+                    try testing.expectEqual(res.result.integer, 0);
+                if (try res.id.eql(99))
+                    try testing.expectEqual(res.err().code, @intFromEnum(ErrorCode.MethodNotFound));
+            }
+        });
+
     }
     if (gpa.detectLeaks()) std.debug.print("Memory leak detected!\n", .{});
 }
