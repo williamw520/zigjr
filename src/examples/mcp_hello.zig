8ba86440a6f5ae52e2679a82976b331e64435e93
William Wong <williamw520@gmail.com>
2025-06-14 15:05:12 -0700

    Complete the mcp_hello example.

--- src/examples/mcp_hello.zig
+++ src/examples/mcp_hello.zig
@@ -9,6 +9,7 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 const Value = std.json.Value;
+const ObjectMap = std.json.ObjectMap;
 const allocPrint = std.fmt.allocPrint;
 
 const zigjr = @import("zigjr");
@@ -31,7 +32,7 @@ pub fn main() !void {
 
         // Register the MCP RPC methods.
         // Pass the logger in as context so each handler can also log to the log file.
-        try handlers.register("initialize", null, mcp_initialize);
+        try handlers.register("initialize", &logger, mcp_initialize);
         try handlers.register("notifications/initialized", &logger, mcp_notifications_initialized);
         try handlers.register("tools/list", &logger, mcp_tools_list);
         try handlers.register("tools/call", &logger, mcp_tools_call);
@@ -50,8 +51,11 @@ pub fn main() !void {
 
 // The MCP message handlers
 
-fn mcp_initialize(params: InitializeRequest_params) InitializeResult {
-    _=params;
+fn mcp_initialize(logger: *zigjr.FileLogger, alloc: Allocator,
+                  params: InitializeRequest_params) !InitializeResult {
+    const msg = try allocPrint(alloc, "client name: {s}, version: {s}", .{params.clientInfo.name, params.clientInfo.version});
+    logger.log("mcp_initialize", "initialize", msg);
+
     return .{
         .protocolVersion = "2025-03-26",    // https://github.com/modelcontextprotocol/modelcontextprotocol/tree/main/schema/2025-03-26
         .serverInfo = Implementation {
@@ -77,34 +81,17 @@ fn mcp_tools_list(logger: *zigjr.FileLogger, alloc: Allocator,
     const msg = try allocPrint(alloc, "params: {any}", .{params});
     logger.log("mcp_tools_list", "tools/list", msg);
     var tools = std.ArrayList(Tool).init(alloc);
-    try tools.append(Tool {
-        .name = "hello",
-        .description = "Replying a 'Hello World' when called.",
-        .inputSchema = .{
-            .properties = try jsonObject(alloc),
-            .required = &.{},       // empty array.
-        },
-    });
 
-    var helloNameParam = try jsonObject(alloc);
-    try helloNameParam.object.put("type", Value { .string = "string" });
-    try helloNameParam.object.put("description", Value { .string = "The name to say hello to" });
-    var helloNameInput = try jsonObject(alloc);
-    try helloNameInput.object.put("name", helloNameParam);
+    const helloTool = Tool.init(alloc, "hello", "Replying a 'Hello World' when called.");
+    try tools.append(helloTool);
 
-    var helloNameInputRequired = std.ArrayList([]const u8).init(alloc);
-    try helloNameInputRequired.append("name");
-    try tools.append(Tool {
-        .name = "hello-name",
-        .description = "Replying a 'Hello NAME' when called with the NAME.",
-        .inputSchema = .{
-            .properties = helloNameInput,
-            .required = helloNameInputRequired.items,
-        },
-    });
+    var helloNameTool = Tool.init(alloc, "hello-name", "Replying a 'Hello NAME' when called with the NAME.");
+    try helloNameTool.inputSchema.addProperty("name", "string", "The name to say hello to");
+    try helloNameTool.inputSchema.addRequired("name");
+    try tools.append(helloNameTool);
 
     return .{
-        .tools = tools.items,
+        .tools = tools,
     };
 }
 
@@ -114,43 +101,33 @@ fn mcp_tools_call(logger: *zigjr.FileLogger, alloc: Allocator, params: Value) !C
     logger.log("mcp_tools_call", "tools/call", msg);
 
     if (std.mem.eql(u8, tool, "hello")) {
-        var contents = std.ArrayList(Content).init(alloc);
-        try contents.append(.{
-            .text = "Hello World",
-            .@"type" = "text",
-        });
-        return .{
-            .content = contents.items,
-            .isError = false,
-        };
+        var ctr = CallToolResult.init(alloc);
+        try ctr.addTextContent("Hello World!");
+        return ctr;
     } else if (std.mem.eql(u8, tool, "hello-name")) {
         const arguments = params.object.get("arguments").?.object;
         const name = arguments.get("name") orelse Value{ .string = "not set" };
         logger.log("mcp_tools_call", "arguments", name.string);
-        var contents = std.ArrayList(Content).init(alloc);
-        try contents.append(.{
-            .text = try allocPrint(alloc, "Hello '{s}'!", .{name.string}),
-            .@"type" = "text",
-        });
-        return .{
-            .content = contents.items,
-            .isError = false,
-        };
+
+        var ctr = CallToolResult.init(alloc);
+        try ctr.addTextContent(try allocPrint(alloc, "Hello '{s}'!", .{name.string}));
+        return ctr;
     } else {
-        var contents = std.ArrayList(Content).init(alloc);
-        try contents.append(.{
-            .text = try allocPrint(alloc, "Tool {s} not found", .{tool}),
-            .@"type" = "text",
-        });
-        return .{
-            .content = contents.items,
-            .isError = true,
-        };
+        var ctr = CallToolResult.init(alloc);
+        try ctr.addTextContent(try allocPrint(alloc, "Tool {s} not found", .{tool}));
+        ctr.isError = true;
+        return ctr;
     }
 }
 
-/// See MCP message schema for detail.
+
+// The following MCP message structs should be put in a library,
+// but it's put here to illustrate that it's not too hard to come up with a MCP example.
+
+/// See the MCP message schema definition and sample messages for detail.
 /// https://github.com/modelcontextprotocol/modelcontextprotocol/tree/main/schema/2025-03-26
+/// https://github.com/modelcontextprotocol/modelcontextprotocol/blob/main/docs/specification/2025-03-26/basic/lifecycle.mdx
+/// https://github.com/modelcontextprotocol/modelcontextprotocol/blob/main/docs/specification/2025-03-26/server/tools.mdx
 
 const InitializeRequest_params = struct {
     protocolVersion:    []const u8,
@@ -200,26 +177,120 @@ const ServerCapabilities = struct {
 };
 
 const ListToolsRequest_params = struct {
-    cursor:     ?[]const u8 = null,
+    cursor:             ?[]const u8 = null,
 };
 
 const ListToolsResult = struct {
-    _meta:      ?Value = null,
-    nextCursor: ?[]const u8 = null,
-    tools:      []Tool,
+    _meta:              ?Value = null,
+    nextCursor:         ?[]const u8 = null,
+    tools:              std.ArrayList(Tool),
+
+    pub fn jsonStringify(self: @This(), jsonWriteStream: anytype) !void {
+        const jws = jsonWriteStream;
+        try jws.beginObject();
+        {
+            try jws.objectField("_meta");
+            try jws.write(self._meta);
+            try jws.objectField("nextCursor");
+            try jws.write(self.nextCursor);
+            try jws.objectField("tools");
+            try jws.write(self.tools.items);
+        }
+        try jws.endObject();
+    }
 };
 
 const Tool = struct {
-    name:           []const u8,
-    description:    ?[]const u8 = null,
-    inputSchema:    InputSchema,
-    annotations:    ?ToolAnnotations = null,
+    name:               []const u8,
+    description:        ?[]const u8 = null,
+    inputSchema:        InputSchema,
+    annotations:        ?ToolAnnotations = null,
+
+    pub fn init(alloc: Allocator, name: []const u8, desc: []const u8) @This() {
+        return .{
+            .name = name,
+            .description = desc,
+            .inputSchema = InputSchema.init(alloc),
+        };
+    }
+
+    pub fn deinit(self: *@This()) void {
+        self.inputSchema.deinit();
+    }
+
 };
 
 const InputSchema = struct {
-    @"type":        [6]u8 = "object".*,
-    properties:     ?Value = null,
-    required:       ?[][]const u8 = null,
+    alloc:              Allocator,
+    @"type":            [6]u8 = "object".*,
+    properties:         Value,
+    required:           std.ArrayList([]const u8),
+
+    pub fn init(alloc: Allocator) @This() {
+        return .{
+            .alloc = alloc,
+            .properties = jsonObject(alloc),
+            .required = std.ArrayList([]const u8).init(alloc),
+        };
+    }
+
+    pub fn deinit(self: *@This()) void {
+        self.properties.object.deinit();
+        self.required.deinit();
+    }
+
+    pub fn jsonStringify(self: @This(), jsonWriteStream: anytype) !void {
+        const jws = jsonWriteStream;
+        try jws.beginObject();
+        {
+            try jws.objectField("type");
+            try jws.write(self.@"type");
+
+            try jws.objectField("properties");
+            try jws.write(self.properties);
+
+            try jws.objectField("required");
+            try jws.write(self.required.items);
+        }
+        try jws.endObject();
+    }
+
+    pub fn addProperty(self: *@This(), prop_name: []const u8, type_name: []const u8, type_desc: []const u8) !void {
+        const typeInfo = try typeObject(self.alloc, type_name, type_desc);
+        try self.properties.object.put(prop_name, typeInfo);
+    }
+
+    pub fn addRequired(self: *@This(), field_name: []const u8) !void {
+        try self.required.append(field_name);
+    }
+
+};
+
+const Annotations = struct {
+    audience:           std.ArrayList(Role),
+    priority:           i64,    // "maximum": 1, "minimum": 0
+
+    pub fn jsonStringify(self: @This(), jsonWriteStream: anytype) !void {
+        const jws = jsonWriteStream;
+        try jws.beginObject();
+        {
+            try jws.objectField("audience");
+            try jws.beginArray();
+            for (self.audience.items)|role| {
+                try jws.write(@tagName(role));
+            }
+            try jws.endArray();
+
+            try jws.objectField("priority");
+            try jws.write(self.priority);
+        }
+        try jws.endObject();
+    }
+};
+
+const Role = enum {
+    assistant,
+    user
 };
 
 const ToolAnnotations = struct {
@@ -231,18 +302,134 @@ const ToolAnnotations = struct {
 };
 
 const CallToolResult = struct {
-    content:        []Content,
-    isError:        bool = false,
+    _meta:              ?Value = null,
+    content:            std.ArrayList(Content),
+    isError:            bool = false,
+
+    pub fn init(alloc: Allocator) @This() {
+        return .{
+            .content = std.ArrayList(Content).init(alloc),
+        };
+    }
+
+    pub fn addTextContent(self: *@This(), text: []const u8) !void {
+        try self.content.append(Content{ .text = TextContent{ .text = text } });
+    }
+    
+    pub fn addImageContent(self: *@This(), data: []const u8, mineType: []const u8) !void {
+        try self.content.append(Content {
+            .image = ImageContent {
+                .data = data,
+                .mimeType = mineType,
+            }
+        });
+    }
+    
+    pub fn addAudioContent(self: *@This(), data: []const u8, mineType: []const u8) !void {
+        try self.content.append(Content {
+            .audio = AudioContent {
+                .data = data,
+                .mimeType = mineType,
+            }
+        });
+    }
+    
+    pub fn jsonStringify(self: @This(), jsonWriteStream: anytype) !void {
+        const jws = jsonWriteStream;
+        try jws.beginObject();
+        {
+            try jws.objectField("_meta");
+            try jws.write(self._meta);
+
+            try jws.objectField("content");
+            try jws.write(self.content.items);
+
+            try jws.objectField("isError");
+            try jws.write(self.isError);
+        }
+        try jws.endObject();
+    }
+    
 };
 
-const Content = struct {
-    text:           []const u8,
-    @"type":        []const u8,
+const Content = union(enum) {
+    text:               TextContent,
+    image:              ImageContent,
+    audio:              AudioContent,
+    embedded:           EmbeddedResource,
+
+    pub fn jsonStringify(self: @This(), jsonWriteStream: anytype) !void {
+        const jws = jsonWriteStream;
+        switch (self) {
+            .text   => |text|   try jws.write(text),
+            .image  => |image|  try jws.write(image),
+            .audio  => |audio|  try jws.write(audio),
+            .embedded => |emb|  try jws.write(emb),
+        }
+    }
+};
+
+const TextContent = struct {
+    @"type":            [4]u8 = "text".*,
+    text:               []const u8,
+    annotations:        ?Annotations = null,
+};
+
+const ImageContent = struct {
+    @"type":            [5]u8 = "image".*,
+    mimeType:           []const u8,
+    data:               []const u8,     // the base64-encoded image data.
+    annotations:        ?Annotations = null,
+};
+
+const AudioContent = struct {
+    @"type":            [5]u8 = "audio".*,
+    mimeType:           []const u8,
+    data:               []const u8,     // the base64-encoded audio data.
+    annotations:        ?Annotations = null,
+};
+
+const EmbeddedResource = struct {
+    @"type":            [8]u8 = "resource".*,
+    resource:           ResourceContents,
+    annotations:        ?Annotations = null,
+};
+
+const ResourceContents = union(enum) {
+    text:               TextResourceContents,
+    blob:               BlobResourceContents,
+
+    pub fn jsonStringify(self: @This(), jsonWriteStream: anytype) !void {
+        const jws = jsonWriteStream;
+        switch (self) {
+            .text   => |text|   try jws.write(text),
+            .blob   => |blob|   try jws.write(blob),
+        }
+    }
+};
+
+const TextResourceContents = struct {
+    mimeType:           ?[]const u8 = null,
+    text:               []const u8,
+    uri:                []const u8,
+};
+
+const BlobResourceContents = struct {
+    mimeType:           ?[]const u8 = null,
+    blob:               []const u8,
+    uri:                []const u8,
 };
 
 
-
-fn jsonObject(alloc: Allocator) !Value {
+fn jsonObject(alloc: Allocator) Value {
     return Value { .object = std.json.ObjectMap.init(alloc) };
 }
+
+fn typeObject(alloc: Allocator, type_name: []const u8, type_desc: []const u8) !Value {
+    var value = jsonObject(alloc);
+    try value.object.put("type", Value { .string = type_name });
+    try value.object.put("description", Value { .string = type_desc });
+    return value;
+}
+
 
