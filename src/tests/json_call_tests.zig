0ff623e3d1304da8287d6fe2302c3d0f7cb85311
William Wong <williamw520@gmail.com>
2025-06-23 18:37:21 -0700

    Clean up json_call_tests.

--- src/tests/json_call_tests.zig
+++ src/tests/json_call_tests.zig
@@ -17,21 +17,27 @@ const JrErrors = zigjr.JrErrors;
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 
 
+var fn0_called = false;
+var fn0_with_err_called = false;
+var fn0_alloc_called = false;
+
 fn fn0() void {
-    std.debug.print("fn0() called\n", .{});
+    // std.debug.print("fn0() called\n", .{});
+    fn0_called = true;
 }
 
 fn fn0_with_err() !void {
-    std.debug.print("fn0_with_err() called\n", .{});
+    // std.debug.print("fn0_with_err() called\n", .{});
+    fn0_with_err_called = true;
 }
 
 fn fn0_return_value() []const u8 {
-    std.debug.print("fn0_return_value() called\n", .{});
+    // std.debug.print("fn0_return_value() called\n", .{});
     return "Hello";
 }
 
 fn fn0_return_value_with_err() ![]const u8 {
-    std.debug.print("fn0_return_value_with_err() called\n", .{});
+    // std.debug.print("fn0_return_value_with_err() called\n", .{});
     return "Hello";
 }
 
@@ -42,43 +48,72 @@ fn fn0_return_json_str() json_call.JsonStr {
 }
 
 fn fn0_alloc(alloc: Allocator) !void {
-    std.debug.print("fn0_alloc() called\n", .{});
+    // std.debug.print("fn0_alloc() called\n", .{});
     // The arena allocator will take care of freeing it.
     _ = try alloc.dupe(u8, "Hello. Allocate some memory without freeing.");
+    fn0_alloc_called = true;
 }
 
 
+var fn1_integer_called = false;
+var fn1_float_called = false;
+var fn1_bool_called = false;
+var fn1_string_called = false;
+var fn1_with_err_called = false;
+var fn1_alloc_with_err_called = false;
+
 fn fn1_integer(a: i64) void {
-    std.debug.print("fn1_integer() called, a:{}\n", .{a});
+    _=a;
+    // std.debug.print("fn1_integer() called, a:{}\n", .{a});
+    fn1_integer_called = true;
 }
 
 fn fn1_float(a: f64) void {
-    std.debug.print("fn1_float() called, a:{}\n", .{a});
+    _=a;
+    // std.debug.print("fn1_float() called, a:{}\n", .{a});
+    fn1_float_called = true;
 }
 
 fn fn1_bool(a: bool) void {
-    std.debug.print("fn1_bool() called, a:{}\n", .{a});
+    _=a;
+    // std.debug.print("fn1_bool() called, a:{}\n", .{a});
+    fn1_bool_called = true;
 }
 
 fn fn1_string(a: []const u8) void {
-    std.debug.print("fn1_string() called, a:{s}\n", .{a});
+    _=a;
+    // std.debug.print("fn1_string() called, a:{}\n", .{a});
+    fn1_string_called = true;
 }
 
 fn fn1_with_err(a: i64) !void {
-    std.debug.print("fn1_with_err() called, a:{}\n", .{a});
+    _=a;
+    // std.debug.print("fn1_with_err() called, a:{}\n", .{a});
+    fn1_with_err_called = true;
 }
 
 fn fn1_alloc_with_err(alloc: Allocator, a: i64) !void {
-    std.debug.print("fn1_alloc_with_err() called, a:{}\n", .{a});
+    _=a;
+    // std.debug.print("fn1_alloc_with_err() called, a:{}\n", .{a});
     // The arena allocator will take care of freeing it.
     _ = try alloc.dupe(u8, "Hello. Allocate some memory without freeing.");
+    fn1_alloc_with_err_called = true;
 }
 
+
+var fn4_called = false;
 
 fn fn4(a: i64, b: f64, c: bool, d: []const u8) void {
-    std.debug.print("fn4_integer() called, a:{}, b:{}, c:{}, d:{s}\n", .{a, b, c, d});
+    _=a;
+    _=b;
+    _=c;
+    _=d;
+    // std.debug.print("fn4_integer() called, a:{}, b:{}, c:{}, d:{s}\n", .{a, b, c, d});
+    fn4_called = true;
 }
 
+
+var fn_cat_called = false;
 
 const CatInfo = struct {
     cat_name: []const u8,
@@ -87,7 +122,9 @@ const CatInfo = struct {
 };
 
 fn fn_cat(a: CatInfo) void {
-    std.debug.print("fn4_integer() called, a:{any}\n", .{a});
+    _=a;
+    // std.debug.print("fn4_integer() called, a:{any}\n", .{a});
+    fn_cat_called = true;
 }
 
 
@@ -98,14 +135,52 @@ test "Test rpc call on fn0." {
         var ctx = {};
         var h = try json_call.makeRpcHandler(&ctx, fn0, alloc);
         defer h.deinit();
+        fn0_called = false;
         _ = try h.invoke(.{ .null = {} });
+        try testing.expect(fn0_called);
+        fn0_called = false;
+
         _ = try h.invokeJson("");
         h.reset();
+        try testing.expect(fn0_called);
+        fn0_called = false;
+
         _ = try h.invoke(.{ .null = {} });
         h.reset();
+        try testing.expect(fn0_called);
+        fn0_called = false;
+
         _ = try h.invoke(.{ .null = {} });
+        h.reset();
+        try testing.expect(fn0_called);
+        fn0_called = false;
+    }
+
+    {
+        var ctx = {};
+        var h = try json_call.makeRpcHandler(&ctx, fn0_with_err, alloc);
+        defer h.deinit();
+        fn0_with_err_called = false;
+
+        _ = try h.invoke(.{ .null = {} });
+        try testing.expect(fn0_with_err_called);
+        fn0_with_err_called = false;
+
+        _ = try h.invoke(.{ .null = {} });
+        try testing.expect(fn0_with_err_called);
+        fn0_with_err_called = false;
+
+        _ = try h.invokeJson("");
+        try testing.expect(fn0_with_err_called);
+        fn0_with_err_called = false;
+
+        const dresult = try h.invoke(.{ .null = {} });
+        try testing.expect(fn0_with_err_called);
+        fn0_with_err_called = false;
+        try testing.expect(dresult == .none);
         h.reset();
     }
+
 
     {
         var ctx = {};
@@ -124,9 +199,19 @@ test "Test rpc call on fn0." {
         var ctx = {};
         var h = try json_call.makeRpcHandler(&ctx, fn0_alloc, alloc);
         defer h.deinit();
+        fn0_alloc_called = false;
+
         _ = try h.invoke(.{ .null = {} });
+        try testing.expect(fn0_alloc_called);
+        fn0_alloc_called = false;
+
         _ = try h.invoke(.{ .null = {} });
+        try testing.expect(fn0_alloc_called);
+        fn0_alloc_called = false;
+
         _ = try h.invokeJson("");
+        try testing.expect(fn0_alloc_called);
+        fn0_alloc_called = false;
         h.reset();
     }
 
@@ -140,22 +225,38 @@ test "Test rpc call on fn1." {
     {
         var h = try json_call.makeRpcHandler(&ctx, fn1_integer, alloc);
         defer h.deinit();
+        fn1_integer_called = false;
+
         _ = try h.invoke(.{ .integer = 123 });
+        try testing.expect(fn1_integer_called);
+        fn1_integer_called = false;
+
         _ = try h.invokeJson("123");
         h.reset();
+        try testing.expect(fn1_integer_called);
+        fn1_integer_called = false;
 
         var array = std.json.Array.init(alloc);
         try array.append(.{ .integer = 456 });
         defer array.deinit();
         _ = try h.invoke(.{ .array = array });
+        try testing.expect(fn1_integer_called);
+        fn1_integer_called = false;
+
         _ = try h.invokeJson("[123]");
         h.reset();
+        try testing.expect(fn1_integer_called);
+        fn1_integer_called = false;
     }
     {
         var h = try json_call.makeRpcHandler(&ctx, fn1_alloc_with_err, alloc);
         defer h.deinit();
         _ = try h.invoke(.{ .integer = 123 });
+        try testing.expect(fn1_alloc_with_err_called);
+        fn1_alloc_with_err_called = false;
+
         _ = try h.invokeJson("123");
+        try testing.expect(fn1_alloc_with_err_called);
     }
 
     {
@@ -171,16 +272,30 @@ test "Test rpc call on fn1." {
         var array = std.json.Array.init(alloc);
         try array.append(.{ .float = 4.56 });
         defer array.deinit();
+        fn1_float_called = false;
+        
         _ = try h.invoke(.{ .array = array });
+        try testing.expect(fn1_float_called);
+        fn1_float_called = false;
+        
         _ = try h.invokeJson("[1.23]");
+        try testing.expect(fn1_float_called);
+        fn1_float_called = false;
         h.reset();
     }
     
     {
         var h = try json_call.makeRpcHandler(&ctx, fn1_bool, alloc);
         defer h.deinit();
+        fn1_bool_called = false;
+
         _ = try h.invoke(.{ .bool = true });
+        try testing.expect(fn1_bool_called);
+        fn1_bool_called = false;
+        
         _ = try h.invokeJson("true");
+        try testing.expect(fn1_bool_called);
+        fn1_bool_called = false;
         h.reset();
     }
     {
@@ -189,16 +304,31 @@ test "Test rpc call on fn1." {
         var array = std.json.Array.init(alloc);
         try array.append(.{ .bool = false });
         defer array.deinit();
+        fn1_bool_called = false;
+
         _ = try h.invoke(.{ .array = array });
+        try testing.expect(fn1_bool_called);
+        fn1_bool_called = false;
+        
         _ = try h.invokeJson("[false]");
+        try testing.expect(fn1_bool_called);
+        fn1_bool_called = false;
+        
         h.reset();
     }
     
     {
         var h = try json_call.makeRpcHandler(&ctx, fn1_string, alloc);
         defer h.deinit();
+        fn1_string_called = false;
+
         _ = try h.invoke(.{ .string = "Hello123" });
+        try testing.expect(fn1_string_called);
+        fn1_string_called = false;
+
         _ = try h.invokeJson("\"Hello123\"");
+        try testing.expect(fn1_string_called);
+        fn1_string_called = false;
         h.reset();
     }
     {
@@ -207,8 +337,16 @@ test "Test rpc call on fn1." {
         var array = std.json.Array.init(alloc);
         try array.append(.{ .string = "Hello456" });
         defer array.deinit();
+        fn1_string_called = false;
+        
         _ = try h.invoke(.{ .array = array });
+        try testing.expect(fn1_string_called);
+        fn1_string_called = false;
+        
         _ = try h.invokeJson("[\"Hello456\"]");
+        try testing.expect(fn1_string_called);
+        fn1_string_called = false;
+        
         h.reset();
     }
 
@@ -222,6 +360,7 @@ test "Test rpc call on fn4." {
         var h = try json_call.makeRpcHandler(&ctx, fn4, alloc);
         defer h.deinit();
         _ = try h.invokeJson("[123, 4.56, true, \"abc\"]");
+        try testing.expect(fn4_called);
         h.reset();
     }
     if (gpa.detectLeaks()) std.debug.print("Memory leak detected!\n", .{});
@@ -240,6 +379,7 @@ test "Test rpc call on fn_cat." {
                 \\ "eye_color": "brown"
                 \\}
         );
+        try testing.expect(fn_cat_called);
         h.reset();
     }
     if (gpa.detectLeaks()) std.debug.print("Memory leak detected!\n", .{});
